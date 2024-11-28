d2f97b4286a9b229a1c06a6fd2bb04ca139382cf
pembem22 <24252798+pembem22@users.noreply.github.com>
2024-11-28 18:31:48 +0200

    Update, add some C++ bindings

--- build.rs
+++ build.rs
@@ -7,13 +7,36 @@ use std::env;
 fn main() {
     let mut config = cmake::Config::new("openvr");
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let target_pointer_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap();
+    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
 
     if target_os == "macos" {
         config.define("BUILD_UNIVERSAL", "OFF");
     } else if target_os == "windows" {
         // Work around broken cmake build.
         config.cxxflag("/DWIN32");
+        // config.cxxflag("/EHsc");
+
+        // // Workaround: Microsoft extensions aren't enabled by default for the `gnu` toolchain
+        // // on Windows. We need to enable it manually, or MSVC headers will fail to parse. We
+        // // also need to manually define architecture macros, or the build will fail with an
+        // // "Unsupported architecture" error.
+        // //
+        // // This does not happen when the `msvc` toolchain is used.
+        // if target_env == "gnu" {
+        //     let arch_macro = match target_arch.as_str() {
+        //         "x86" => "_M_IX86",
+        //         "x86_64" => "_M_X64",
+        //         "arm" => "_M_ARM",
+        //         "aarch64" => "_M_ARM64",
+        //         _ => panic!("architecture {target_arch} not supported on Windows"),
+        //     };
+
+        //     config.build_arg("-fms-extensions");
+        //     config.build_arg("-fmsc-version=1300");
+        //     config.build_arg(format!("-D{arch_macro}=100"));
+        // }
     }
 
     let dst = config.build();
@@ -39,6 +62,7 @@ fn main() {
         .header("wrapper.hpp")
         .constified_enum(".*")
         .prepend_enum_name(false)
+        .vtable_generation(true)
         .generate()
         .expect("could not generate bindings")
         .write_to_file("bindings.rs")
--- wrapper.hpp
+++ wrapper.hpp
@@ -29,3 +29,58 @@ struct VRTextureWithPose_t_real
 	EColorSpace eColorSpace;
 	HmdMatrix34_t mDeviceToAbsoluteTracking; // Actual pose used to render scene textures.
 };
+
+/** This interface is provided by vrserver to allow the driver to notify
+ * the system when something changes about a device. These changes must
+ * not change the serial number or class of the device because those values
+ * are permanently associated with the device's index. */
+class IVRDriverContext
+{
+public:
+  /** Returns the requested interface. If the interface was not available it will return NULL and fill
+   * out the error. */
+  virtual void *GetGenericInterface(const char *pchInterfaceVersion, EVRInitError *peError = nullptr) = 0;
+
+  /** Returns the property container handle for this driver */
+  virtual DriverHandle_t GetDriverHandle() = 0;
+};
+
+const char *IServerTrackedDeviceProvider_Version = "IServerTrackedDeviceProvider_004";
+
+/** This interface must be implemented in each driver. It will be loaded in vrserver.exe */
+class IServerTrackedDeviceProvider
+{
+public:
+  /** initializes the driver. This will be called before any other methods are called.
+   * If Init returns anything other than VRInitError_None the driver DLL will be unloaded.
+   *
+   * pDriverHost will never be NULL, and will always be a pointer to a IServerDriverHost interface
+   *
+   * pchUserDriverConfigDir - The absolute path of the directory where the driver should store user
+   *	config files.
+   * pchDriverInstallDir - The absolute path of the root directory for the driver.
+   */
+  virtual EVRInitError Init(IVRDriverContext *pDriverContext) = 0;
+
+  /** cleans up the driver right before it is unloaded */
+  virtual void Cleanup() = 0;
+
+  /** Returns the version of the ITrackedDeviceServerDriver interface used by this driver */
+  virtual const char *const *GetInterfaceVersions() = 0;
+
+  /** Allows the driver do to some work in the main loop of the server. */
+  virtual void RunFrame() = 0;
+
+  // ------------  Power State Functions ----------------------- //
+
+  /** Returns true if the driver wants to block Standby mode. */
+  virtual bool ShouldBlockStandbyMode() = 0;
+
+  /** Called when the system is entering Standby mode. The driver should switch itself into whatever sort of low-power
+   * state it has. */
+  virtual void EnterStandby() = 0;
+
+  /** Called when the system is leaving Standby mode. The driver should switch itself back to
+  full operation. */
+  virtual void LeaveStandby() = 0;
+};
